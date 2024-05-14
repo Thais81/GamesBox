a2ba2856965a4bfbfa377af2a441bddcb46eda9c
Eden <federico.manca.14@gmail.com>
2024-05-14 13:56:47 +0200

    Game interface updated

--- src/main/java/entities/Game.java
+++ src/main/java/entities/Game.java
@@ -1,9 +1,16 @@
-
 package entities;
+
+import java.io.Serializable;
 
 /**
  * @author Federico Manca
  */
-public class Game {
 
+@SuppressWarnings("serial")
+public interface Game extends Serializable {
+    //No variables as it's an interface (can only have constant variables)
+    Integer getIdGame();
+    String getGameName();
+    void start();
+    boolean win();
 }
