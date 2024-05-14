8c56ccf4f8c19ce4f00e4ca0141803cefc6a6e81
FMANCA <federico.manca.14@gmail.com>
2024-05-14 08:45:02 +0200

    Initial Commit: Java 17 with Spring Current dependencies: -MariaDB Driver -Spring Data JPA -Spring Boot DevTools

---
+++ src/main/java/fr/ldnr/groupe4/boitedejeux/BoitedejeuxApplication.java
@@ -0,0 +1,13 @@
+package fr.ldnr.groupe4.boitedejeux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoitedejeuxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoitedejeuxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/ldnr/groupe4/boitedejeux/BoitedejeuxApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.ldnr.groupe4.boitedejeux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoitedejeuxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
