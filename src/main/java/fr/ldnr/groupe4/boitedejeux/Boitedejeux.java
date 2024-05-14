f18ccaf1999f2c45774d9071d5f8af04d751543f
FMANCA <federico.manca.14@gmail.com>
2024-05-14 11:28:42 +0200

    Initial commit: Java 1.8 with Maven Dependencies: H2 Hibernate Eclipse

---
+++ src/main/java/fr/ldnr/groupe4/boitedejeux/Boitedejeux.java
@@ -0,0 +1,13 @@
+
+package fr.ldnr.groupe4.boitedejeux;
+
+/**
+ *
+ * @author Federico Manca
+ */
+public class Boitedejeux {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
