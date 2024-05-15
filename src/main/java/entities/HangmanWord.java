a6f18e591c6f3ee71e9c85d037eba0721a2d2876
Thaïs Genin <genin.thais@gmail.com>
2024-05-16 00:24:10 +0200

    DAOmanager, Singleton mariaDB et DAOFactory DAO et leur entity ok avec CRUD
    Tests non faits
    
    améliorations à venir:
    USER ET PASSWORD en clair dans le code
    Pour la création GAME ou USER ou Play --→ utilisation de LIST(), serait moins lourd avec StringBuilder() ?
    Gestion des exceptions ???
           «  } catch (SQLException e) {
       System.out.println("Erreur lors de l'insertion: " + e.getMessage());
            } »

---
+++ src/main/java/entities/HangmanWord.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entities;
+
+/**
+ *
+ * @author Thaïs GENIN
+ */
+public class HangmanWord {
+    private int id_HangmanWord;
+    private String guessed_word;
+
+    public HangmanWord() {
+    }
+
+    public HangmanWord(int id_HangmanWord, String guessed_word) {
+        this.id_HangmanWord = id_HangmanWord;
+        this.guessed_word = guessed_word;
+    }
+
+    public int getId_HangmanWord() {
+        return id_HangmanWord;
+    }
+
+    public void setId_HangmanWord(int id_HangmanWord) {
+        this.id_HangmanWord = id_HangmanWord;
+    }
+
+    public String getGuessed_word() {
+        return guessed_word;
+    }
+
+    public void setGuessed_word(String guessed_word) {
+        this.guessed_word = guessed_word;
+    }
+
+}
---
+++ src/main/java/entities/Play.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entities;
+
+/**
+ *
+ * @author Thaïs GENIN
+ */
+public class Play {
+    private int id_user;
+    private int id_game;
+    private int nb_win;
+    private int nb_playedgames;
+
+    public Play() {
+
+    }
+    public Play(int id_user, int id_game, int nb_win, int nb_playedgames) {
+        this.id_user = id_user;
+        this.id_game = id_game;
+        this.nb_win = nb_win;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Play{");
+        sb.append("id_user=").append(id_user);
+        sb.append(", id_game=").append(id_game);
+        sb.append(", nb_win=").append(nb_win);
+        sb.append(", nb_playedgames=").append(nb_playedgames);
+        sb.append('}');
+        return sb.toString();
+    }
+
+    public int getId_user() {
+        return id_user;
+    }
+
+    public int getId_game() {
+        return id_game;
+    }
+
+    public int getNb_win() {
+        return nb_win;
+    }
+
+    public void setNb_win(int nb_win) {
+        this.nb_win = nb_win;
+    }
+
+    public int getNb_playedgames() {
+        return nb_playedgames;
+    }
+
+    public void setNb_playedgames(int nb_playedgames) {
+        this.nb_playedgames = nb_playedgames;
+    }
+
+}
